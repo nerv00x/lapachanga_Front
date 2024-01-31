1bd41671a95326fa7b62ae05f993290aba85c7ae
aridane <aridanecabreraramirez@gmail.com>
2024-01-31 19:43:33 +0100

    login funcional

--- src/Componentes/LoginForm.jsx
+++ src/Componentes/LoginForm.jsx
@@ -1,53 +1,126 @@
 // LoginForm.js
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import RegisterModal from "./registerModal";
-import { useState } from "react";
-
+import LoginModal from "./loginModal"; 
 
 const LoginForm = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Lógica de inicio de sesión
-  };
-
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showRegisterModal, setShowRegisterModal] = useState(false);
-  const handleOpenRegisterModal = () => {
-    setShowRegisterModal(true);
-  };
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+
+
   const handleCloseRegisterModal = () => {
     setShowRegisterModal(false);
   };
 
+  const handleCloseLoginModal = () => {
+    setShowLoginModal(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Realizar la solicitud POST a /login
+      const response = await axios.post(
+        "http://lapachanga-back.test/api/login",
+        {
+          email: email,
+          password: password,
+        }
+      );
+
+      // Manejar la respuesta exitosa
+      console.log("Inicio de sesión exitoso:", response.data);
+
+      // Actualizar el estado para mostrar el mensaje de éxito
+      setSuccessMessage("Inicio de sesión exitoso. ¡Bienvenido!");
+
+      // Cerrar el modal de inicio de sesión después del inicio de sesión exitoso
+      handleCloseLoginModal();
+
+      // Redirigir a la página deseada después del inicio de sesión exitoso
+      navigate("/directo");
+    } catch (error) {
+      // Manejar errores (puedes mostrar un mensaje de error al usuario, etc.)
+      setErrorMessage("Error al iniciar sesión");
+      console.error("Error al iniciar sesión:", error);
+    }
+  };
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
+    <div>
+      {successMessage && (
+        <Alert
+          variant="success"
+          onClose={() => setSuccessMessage(null)}
+          dismissible
+        >
+          {successMessage}
+        </Alert>
+      )}
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+      {errorMessage && (
+        <Alert
+          variant="danger"
+          onClose={() => setErrorMessage(null)}
+          dismissible
+        >
+          {errorMessage}
+        </Alert>
+      )}
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
 
-      <Button
-        variant="success"
-        className="ms-2"
-        onClick={handleOpenRegisterModal}
-      >
-        Register
-      </Button>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
 
-      <RegisterModal
-        showModal={showRegisterModal}
-        handleClose={handleCloseRegisterModal}
-      />
-    </Form>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+
+        <Button
+          variant="success"
+          className="ms-2"
+          onClick={() => setShowLoginModal(true)} // Abre el modal de inicio de sesión
+        >
+          Register
+        </Button>
+
+        <RegisterModal
+          showModal={showRegisterModal}
+          handleClose={handleCloseRegisterModal}
+        />
+
+        <LoginModal
+          showModal={showLoginModal}
+          handleClose={handleCloseLoginModal}
+        />
+      </Form>
+    </div>
   );
 };
 
--- src/Componentes/registerModal.jsx
+++ src/Componentes/registerModal.jsx
@@ -1,14 +1,38 @@
 // RegisterModal.js
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import axios from "axios";
 
 function RegisterModal({ showModal, handleClose }) {
-  const handleRegister = () => {
-    // Lógica de registro, puedes implementarla según tus necesidades
-    // Por ahora, simplemente cerramos el modal
-    handleClose();
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPassword_confirmation] = useState("");
+
+  const handleRegister = async () => {
+    try {
+      // Realizar la solicitud POST para el registro
+      const response = await axios.post(
+        "http://lapachanga-back.test/api/register",
+        {
+          nombre: nombre,
+          email: email,
+          password: password,
+          password_confirmation: password,
+        }
+      );
+
+      // Manejar la respuesta exitosa (puedes realizar acciones adicionales, como redireccionar)
+      console.log("Registro exitoso:", response.data);
+
+      // Cerrar el modal después de un registro exitoso
+      handleClose();
+    } catch (error) {
+      // Manejar errores (puedes mostrar un mensaje de error al usuario, etc.)
+      console.error("Error al registrar:", error);
+    }
   };
 
   return (
@@ -18,16 +42,48 @@ function RegisterModal({ showModal, handleClose }) {
       </Modal.Header>
       <Modal.Body>
         {/* Formulario de Registro */}
+
         <Form>
           {/* Agrega campos de registro según tus necesidades */}
+
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="nombre"
+              placeholder="Enter Name"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password Confirm</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password_confirmation}
+              onChange={(e) => setPassword_confirmation(e.target.value)}
+            />
           </Form.Group>
 
           {/* Agrega más campos según sea necesario */}
@@ -42,7 +98,6 @@ function RegisterModal({ showModal, handleClose }) {
         </Button>
       </Modal.Footer>
     </Modal>
-    
   );
 }
 
